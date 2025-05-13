22bc17aadc86cd9abc94c4a30e2c85460d5a877b
Zhangyao719 <1258858403@qq.com>
2025-05-13 23:55:05 +0800

    feat: @Resource = @Autowired + @Qualifier

--- web-manager/src/main/java/com/manager/controller/DeptController.java
+++ web-manager/src/main/java/com/manager/controller/DeptController.java
@@ -2,6 +2,7 @@ package com.manager.controller;
 
 import com.manager.entity.Result;
 import com.manager.service.DeptService;
+import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,8 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class DeptController {
-    @Autowired // 自动从 IOC 容器中寻找 Bean 对象，为该变量赋值
-    @Qualifier("service2")
+    // @Autowired // 自动从 IOC 容器中寻找 Bean 对象，为该变量赋值
+    // @Qualifier("service2")
+    @Resource(name = "service2") // JDK 自带，默认按 Bean 的首字母小写名称注入（也可根据 @Service 指定的名字注入）
     private DeptService deptService;
 
     // @RequestMapping(value = "/depts", method = RequestMethod.GET)
