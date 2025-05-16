342eaf160bc0f4234e30a3b83de0097b64ff2710
Zhangyao719 <1258858403@qq.com>
2025-05-16 17:21:44 +0800

    feat: 接入 JDBC

---
+++ web-manager/src/test/java/com/manager/JDBCTest.java
@@ -0,0 +1,38 @@
+package com.manager;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * JDBC 测试
+ */
+public class JDBCTest {
+    @Test
+    public void testUpdate() throws ClassNotFoundException, SQLException {
+        // 1. 加载 MySQL 驱动（旧方式，现代 JDBC 通常不需要显式调用）
+        Class.forName("com.mysql.cj.jdbc.Driver"); // JDBC 驱动的全限定名
+
+        // 2. 获取数据库连接对象 Connection
+        Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/users", // 数据库地址
+                "root",
+                "1234"
+        );
+
+        // 3. 获取 SQL 语句执行对象
+        Statement statement = connection.createStatement();
+
+        // 4. 执行 SQL 语句
+        // 该方法返回 int 值，表示影响的行数，根据返回值验证执行结果。
+        int row = statement.executeUpdate("update employee set qq = 1234567899 where id = 1");
+        System.out.println("row = " + row);
+
+        // 5. 释放资源
+        statement.close();
+        connection.close();
+    }
+}
