1c55a87b36885f32fb16378a77d6e456930245ce
Zhangyao719 <1258858403@qq.com>
2025-05-18 23:38:21 +0800

    feat: XML 配置 SQL 映射

--- web-manager/src/main/java/com/manager/mapper/EmployeeMapper.java
+++ web-manager/src/main/java/com/manager/mapper/EmployeeMapper.java
@@ -9,6 +9,7 @@ import java.util.List;
 @Mapper // 作用：程序启动时，自动生成该接口的代理对象，交由 IOC 容器管理
 public interface EmployeeMapper {
     // 查询所有用户数据
-    @Select("select * from employee")
+    // 方式一：@Select("select * from employee")
+    // 方式二：XML 配置（见 resources/com/manager/mapper/EmployeeMapper.xml）
     public List<Employee> getAllEmployees();
 }
