2a83bdfc8188786a3c3a23991435aeb6582bb74c
Zhangyao719 <1258858403@qq.com>
2025-05-29 17:13:26 +0800

    feat: 创建员工信息

--- web-manager/src/main/java/com/manager/entity/Employee.java
+++ web-manager/src/main/java/com/manager/entity/Employee.java
@@ -4,7 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -17,9 +19,16 @@ public class Employee {
     private Integer gender; // 1- 男，2-女
     private String phone;
     private Integer deptId;
-    private String deptName;
     private Integer job; // 1-班主任，2-讲师，3-学工主管，4-教研主管，5-咨询师
     private Double salary; // 薪资
     private String avatar; // 头像链接
-    private LocalDateTime entryDate; // 入职时间
+    private LocalDate entryDate; // 入职时间
+    private LocalDateTime createTime; // 创建时间
+    private LocalDateTime updateTime; // 更新时间
+
+    // 从 department 表中查询到的部门名称
+    private String deptName;
+
+    // 从 experience 表中查询到的员工经历
+    private List<EmployeeExperience> experienceList;
 }
--- web-manager/src/main/java/com/manager/service/EmployeeService.java
+++ web-manager/src/main/java/com/manager/service/EmployeeService.java
@@ -1,5 +1,6 @@
 package com.manager.service;
 
+import com.manager.entity.Employee;
 import com.manager.entity.EmployeeQueryParam;
 import com.manager.entity.PageBean;
 
@@ -7,4 +8,5 @@ public interface EmployeeService {
     PageBean page(Integer page, Integer pageSize);
     PageBean list(Integer page, Integer pageSize);
     PageBean queryPage(EmployeeQueryParam params);
+    void save(Employee employee);
 }
