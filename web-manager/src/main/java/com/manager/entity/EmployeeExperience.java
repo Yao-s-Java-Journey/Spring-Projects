ad07c601841a5b738e36260278f3993a52ec9c3a
Zhangyao719 <1258858403@qq.com>
2025-05-30 14:58:52 +0800

    feat: `@Options` 注解与 `<foreach>` SQL 标签

--- web-manager/src/main/java/com/manager/entity/EmployeeExperience.java
+++ web-manager/src/main/java/com/manager/entity/EmployeeExperience.java
@@ -9,6 +9,8 @@ import java.time.LocalDate;
  */
 @Data
 public class EmployeeExperience {
+    private Integer id;
+    private Integer employeeId; // 对应的员工 id
     private String company; // 公司
     private String job; // 职位
     private LocalDate begin;
