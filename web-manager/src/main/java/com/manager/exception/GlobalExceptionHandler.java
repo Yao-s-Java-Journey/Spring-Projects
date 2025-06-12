a5ea083ee3c8c75ea3200dbc677fea9eccf08d06
Zhangyao719 <1258858403@qq.com>
2025-06-13 00:04:00 +0800

    wip: `@RestControllerAdvice` 与 `@ExceptionHandler`

--- web-manager/src/main/java/com/manager/exception/GlobalExceptionHandler.java
+++ web-manager/src/main/java/com/manager/exception/GlobalExceptionHandler.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
-@RestControllerAdvice
+@RestControllerAdvice // 捕获 Controller 控制器层抛出的所有异常
 public class GlobalExceptionHandler {
+    // 指定处理何种异常
     @ExceptionHandler
+    // @ExceptionHandler(NullPointerException.class)
     public Result doException(Exception e) {
         log.error("服务器发生异常：{}", e.getMessage());
         return Result.error("服务器发生异常");
