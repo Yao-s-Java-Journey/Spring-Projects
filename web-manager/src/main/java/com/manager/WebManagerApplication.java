cf30d20c54066ef4f8925fa54f71d5e0d0e17a0a
Zhangyao719 <1258858403@qq.com>
2025-06-16 14:33:47 +0800

    feat: Filter 过滤器与拦截校验

--- web-manager/src/main/java/com/manager/WebManagerApplication.java
+++ web-manager/src/main/java/com/manager/WebManagerApplication.java
@@ -2,8 +2,10 @@ package com.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan // 开启 Servlet 组件扫描
 public class WebManagerApplication {
 
 	public static void main(String[] args) {
---
+++ web-manager/src/main/java/com/manager/filter/LoginFilter.java
@@ -0,0 +1,58 @@
+package com.manager.filter;
+
+import com.manager.utils.JWT;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.*;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+
+/**
+ * 登录过滤器
+ */
+@Slf4j
+@WebFilter("/*") // urlPatterns 和 values 作用一样，可以只写一个
+public class LoginFilter implements Filter{
+    @Override
+    public void doFilter(
+            ServletRequest request,
+            ServletResponse response,
+            FilterChain chain
+    ) throws ServletException, IOException {
+        // 1. 强转 request 和 response（以便使用部分方法）
+        HttpServletRequest  req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+
+        // 2. 获取 uri，判断登录接口
+        String uri = req.getRequestURI(); // uri 和 url 的区别：url 包含了协议、域名、端口号，uri 只包含路径
+        if (uri.contains("login")) {
+            // 2.1 登录接口直接放行
+            chain.doFilter(request, response);
+        } else {
+            // 2.2 其他接口进行 token 登录验证
+            String token = req.getHeader("Authorization");
+
+            // 2.3 令牌非空判断
+            if (token == null || token.isEmpty()) {
+                resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            };
+
+            // 2.4 解析令牌
+            try  {
+                Claims claims = JWT.parseToken(token);
+                log.info("=== claims === {}", claims);
+                chain.doFilter(request, response);
+            } catch (Exception e) {
+                log.error("解析出错了：{}", e.getMessage());
+                resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            }
+        }
+    }
+}
